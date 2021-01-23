15ccf148696ae844508ec81c67d660bc40856d4c
unknown <zakaria.khalil1999@gmail.com>
2021-01-23 11:25:26 +0100

    first commit

---
+++ sourceFolder1/sourceFolder2/data.js
@@ -0,0 +1,5 @@
+export const data=[{key:'1',nom:'Yagourt', quantite:'25', prixunit:'2'},
+{key:'2',nom:'Milk', quantite:'25', prixunit:'4'},
+{key:'3',nom:'Flowers', quantite:'4', prixunit:'12'},
+{key:'4',nom:'Bread', quantite:'4', prixunit:'5'}
+];
